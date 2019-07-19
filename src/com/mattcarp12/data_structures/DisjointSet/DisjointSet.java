a7ad4cb3ce776a8a37121480d7ba4edf0ef288f8
mcarpenter <mattcarp88@gmail.com>
2019-07-19 08:06:49 -0400

    update

--- src/com/mattcarp12/data_structures/DisjointSet/DisjointSet.java
+++ src/com/mattcarp12/data_structures/DisjointSet/DisjointSet.java
@@ -1,9 +1,20 @@
 package com.mattcarp12.data_structures.DisjointSet;
 
+/**
+ * Implements a disjoint set data structure with
+ * weighted find (to keep tree height low) and
+ * path compression (to keep tree height low).
+ */
 public class DisjointSet {
     int[] parent;
     int[] weight;
 
+    /**
+     * Creates a new disjoint union object. Initially
+     * all objects are disjoint and thus each has
+     * weight 1.
+     * @param size - total number of items.
+     */
     public DisjointSet(int size) {
         parent = new int[size];
         weight = new int[size];
@@ -13,21 +24,39 @@ public class DisjointSet {
         }
     }
 
+    /**
+     * Find the parent node of the given node.
+     * Implements path compression by setting the
+     * parent of each node on the path to the ultimate
+     * root node.
+     * @param i - the node to find the root of
+     * @return index of the root in the parent array
+     */
     public int find(int i) {
         if (parent[i] == -1) return i;
         parent[i] = find(parent[i]);
         return parent[i];
     }
 
+    /**
+     * Merge two disjoint sets using weighted join.
+     * The set with the smaller weight is merged
+     * to the set with larger weight, which minimizes
+     * tree height.
+     * @param i - first node
+     * @param j - second node
+     */
     public void merge(int i, int j) {
         int root1 = find(i);
         int root2 = find(j);
-        if (weight[root1] < weight[root2]) {
-            parent[root1] = root2;
-            weight[root2] += weight[root1];
-        } else {
-            parent[root2] = root1;
-            weight[root1] += weight[root2];
+        if (root1 != root2) {
+            if (weight[root1] < weight[root2]) {
+                parent[root1] = root2;
+                weight[root2] += weight[root1];
+            } else {
+                parent[root2] = root1;
+                weight[root1] += weight[root2];
+            }
         }
     }
 }
