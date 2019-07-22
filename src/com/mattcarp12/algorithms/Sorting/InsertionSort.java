e28944cd263895495ba1062746cf874d7ed0119e
mcarpenter <mattcarp88@gmail.com>
2019-07-22 06:53:33 -0400

    implement insertion sort

---
+++ src/com/mattcarp12/algorithms/Sorting/InsertionSort.java
@@ -0,0 +1,26 @@
+package com.mattcarp12.algorithms.Sorting;
+
+import java.util.Random;
+
+public class InsertionSort {
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i - 1;
+            int key = arr[i];
+            while (j >= 0 && arr[j] > key) {
+                arr[j+1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < 20; i++) arr[i] = random.nextInt(100);
+        insertionSort(arr);
+        for (int num : arr) System.out.print(num + " ");
+    }
+}
